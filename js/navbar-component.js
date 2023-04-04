a1695d930bde737cbd646e5303fe96e33c8bd5a5
moha69210 <mohamedchettah@gmail.com~>
2023-04-04 17:03:52 +0200

    directory images + navbar

--- js/navbar-component.js
+++ js/navbar-component.js
@@ -1,43 +1,27 @@
 class NavbarComponent extends HTMLElement {
-  constructor() {
-      super();
-      this.attachShadow({ mode: 'open' });
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+    }
+  
+    connectedCallback() {
+        this.render();
+    }
+  
+    render() {
+        this.shadowRoot.innerHTML = `
+            <style>
+            @import url('../src/input.css');
+            </style>
+            <nav class ="bg-blue-100">
+                <img class="logo" src="../images/logo/logo_emma_pierre.png"></img>
+                <a href="/">Accueil</a>
+                <a href="/about">À propos</a>
+                <a href="/contact">Contact</a>
+            </nav>
+        `;
+    }
   }
-
-  connectedCallback() {
-      this.render();
-  }
-
-  render() {
-      this.shadowRoot.innerHTML = `
-          <style>
-              .navbar {
-                  display: flex;
-                  justify-content: space-between;
-                  align-items: center;
-                  background-color: #333;
-                  padding: 1rem;
-                  font-family: Arial, sans-serif;
-              }
-
-              .navbar a {
-                  color: white;
-                  text-decoration: none;
-                  padding: 0.5rem;
-              }
-
-              .navbar a:hover {
-                  background-color: #555;
-                  border-radius: 5px;
-              }
-          </style>
-          <nav class="navbar">
-              <a href="/">Accueil</a>
-              <a href="/about">À propos</a>
-              <a href="/contact">Contact</a>
-          </nav>
-      `;
-  }
-}
-
-customElements.define('navbar-component', NavbarComponent);
+  
+  customElements.define('navbar-component', NavbarComponent);
+  
