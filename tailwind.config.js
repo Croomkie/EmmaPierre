680f3688445660d279b9b30963bf5f71cabf9407
Lucas <a@a.fr>
2023-04-05 10:08:08 +0200

    Tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.{html,js}"],
+  content: ["*.{html,js}", "view/*.{html,js}"],
   theme: {
     extend: {
       colors: {
