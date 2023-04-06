09360833b2ac2ed380f69df9a24f71e918f1f69d
Romain Grange <romain.grange@pixlstudio.fr>
2023-04-06 13:12:49 +0200

    add script for flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.{html,js}", "view/*.{html,js}"],
+  content: [
+    "*.{html,js}",
+    "view/*.{html,js}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
     extend: {
       colors: {
